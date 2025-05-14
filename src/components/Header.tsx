d65fe65958dfa55c3c50ffdd28a9ff69d7396d49
snus4me <mathias.hun@gmail.com>
2025-05-14 22:44:17 +0200

    added animation to color theme switcher

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import styles from './Header.module.scss';
 import { flushSync } from 'react-dom';
 import lightmodeSVG from '../assets/lightMode.svg';
 import darkmodeSVG from '../assets/darkMode.svg';
+import { motion, AnimatePresence } from 'motion/react';
 
 interface Props {
   isDarkMode: boolean;
@@ -26,9 +27,25 @@ const Header = ({ isDarkMode, setIsDarkMode }: Props) => {
       <button className={styles.mode_wrapper} onClick={toggleDarkMode}>
         <span className={styles.svgWrapper}>
           {isDarkMode ? (
-            <img src={lightmodeSVG} alt="" />
+            <motion.img
+              src={lightmodeSVG}
+              alt=""
+              initial={{ rotate: -360 }}
+              exit={{ rotate: 360 }}
+              animate={{ rotate: 0 }}
+              transition={{ duration: 0.5 }}
+              key={1}
+            />
           ) : (
-            <img src={darkmodeSVG} alt="" />
+            <motion.img
+              src={darkmodeSVG}
+              alt=""
+              initial={{ rotate: -360 }}
+              animate={{ rotate: 0 }}
+              exit={{ rotate: 360 }}
+              transition={{ duration: 0.5 }}
+              key={2}
+            />
           )}
         </span>
         <span>{renderModeText()}</span>
